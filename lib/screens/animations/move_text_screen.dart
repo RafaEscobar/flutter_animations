f439854539c61405db16954f4b2271a27ca2225e
Rafael Escobar <rescobar@bit.lat>
2025-05-22 12:18:41 -0600

    Agrega correcciones de ortografía

--- lib/screens/animations/move_text_screen.dart
+++ lib/screens/animations/move_text_screen.dart
@@ -4,26 +4,30 @@ class MoveTextScreen extends StatelessWidget{
   const MoveTextScreen({super.key});
   static const String routeName = 'move-text-screen';
 
+  //* 1. Definimos un build con un CustomScrollView y un slivers como ya hemos visto previamente
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: _MoveTextScreenDelegate(maxExtent: 200, minExtent: 120)
-          ),
-          SliverFillRemaining(
-            child: Container(
-              color: Colors.blue,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: _MoveTextScreenDelegate(maxExtent: 200, minExtent: 120)
             ),
-          )
-        ],
+            SliverFillRemaining(
+              child: Container(
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
+//* 2. La clase que hereda de SliverPersistentHeaderDelegate igual permanece sin cambios
 class _MoveTextScreenDelegate extends SliverPersistentHeaderDelegate {
   _MoveTextScreenDelegate({
     required this.maxExtent,
@@ -34,21 +38,27 @@ class _MoveTextScreenDelegate extends SliverPersistentHeaderDelegate {
   @override
   final double minExtent;
 
+  //* 3. El método buil es el que si sufre cambios:
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent){
+    //* 3.1 Definimos con normalidad el currentHeight para la altura dinámica del contenedor
     final double currentHeight = (maxExtent - shrinkOffset).clamp(minExtent, maxExtent);
+    //* 3.2 Obtenemos el rango disponible (altura máxima - altura máxima): Ejemplo: 200 - 120 = 80
     final fadeRange = maxExtent - minExtent;
+    //* 3.3 Es un valor normalizado entre 0 y 1 que va aumentando o disminuyendo con base el scroll y el fadeRange
+    //* el cual usamos para dar un valor dinámico a la coordenada x del Alingment y así desplazar el texto
     final t = (shrinkOffset / fadeRange).clamp(0.0, 1.0);
-    //* t: (20 / 80) == .25
-    //* fontSize: (24 - (24 - 12)) = 12 | 12 * .25 = 21
-    // !
-    //* t: 60 / 80 == .75
-    //* fontSize: (24 - (24 - 12)) = 12 | 12 * .75 = 9 (9 se devuelve como 12 por el clamp) == 12
+
+    //* 3.4 Definimos tamaño de fuente minimo y un tamaño máximo
     final maxSize = 24;
     final minSize = 12;
     final fontSize = maxSize - (maxSize - minSize) * t;
 
-    final alignmentX = -t;
+    //* 3.5 Almacenamos en una variable independiente los valores de las coordenadas X para el texto 1 y el 2
+    //* El texto 1 ira hacia la izq es por eso que t se toma como negativo (desplazamiento de x hacia la izq)
+    //* El texto 2 ira hacia la derecha es por eso que t se toma como positivo (desplazamiento de x hacia la derecha)
+    final text1X = -t;
+    final text2X = t;
 
     return Container(
       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
@@ -57,20 +67,20 @@ class _MoveTextScreenDelegate extends SliverPersistentHeaderDelegate {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          //* fadeRange: 200 - 120 = 80
-          //* t: (20/80) = 0.25
-          //* alignmentX = -0.25
-          //* Alignment(-0.25, 0), -> Se desplaza ese porcentaje hacia la izquierda ya que x+ => derecha | x- => izquierda
+          //* 4 Envolvemos los Text en Align y definimos un Alignment(x, y) para pasarle el valor dinámico de X y así puedan desplazarse según el scroll
+          //* 5 Así mismo pasamos el tamaño dinámico del texto para que reduzca o vuelva a su tamaño normal según corresponda.
           Align(
-            alignment: Alignment(alignmentX, 0),
-            child: Text('Título', style: TextStyle(fontSize: fontSize),)
+            alignment: Alignment(text1X, 0),
+            child: Text('<- Hacía la izq', style: TextStyle(fontSize: fontSize),)
           ),
-          SizedBox(width: 12),
+          SizedBox(height: 4),
           Align(
-            alignment: Alignment(alignmentX * -1, 0),
-            child: Text('Subtítulo'),
+            alignment: Alignment(text2X, 0),
+            child: Text('Hacia la der ->'),
           ),
-          Text(alignmentX.toStringAsFixed(2))
+          SizedBox(height: 12),
+          Text("Valor de X para texto 1: (${text1X.toStringAsFixed(2)}, 0)"),
+          Text("Valor de X para texto 2: (${text2X.toStringAsFixed(2)}, 0)")
         ],
       ),
     );
